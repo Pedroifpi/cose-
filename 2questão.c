7066b50caa53c8b27017992349ceec4029e41000
pedro lucas <160865525+Pedroifpi@users.noreply.github.com>
2026-04-30 15:02:25 -0300

    Atualizar o 2questão.c

--- 2questão.c
+++ 2questão.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+    char c;
+
+    printf("Digite um caractere: ");
+    scanf(" %c", &c);
+
+    switch (c >= 'a' && c <= 'z') {
+        case 1:
+            printf("É letra minúscula\n");
+            break;
+        default:
+            printf("Não é letra minúscula\n");
+    }
+
+    return 0;
+}
