3d373506eb338db4371a5762966a3e15161d5c9d
pedro lucas <160865525+Pedroifpi@users.noreply.github.com>
2026-04-30 15:04:02 -0300

    Atualizar o 5questão.c

--- 5questão.c
+++ 5questão.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+int main() {
+    float a, b;
+    char op;
+
+    printf("Digite (ex: 10 + 2): ");
+    scanf("%f %c %f", &a, &op, &b);
+
+    switch (op) {
+        case '+':
+            printf("Resultado: %.2f\n", a + b);
+            break;
+        case '-':
+            printf("Resultado: %.2f\n", a - b);
+            break;
+        case '*':
+            printf("Resultado: %.2f\n", a * b);
+            break;
+        case '/':
+            if (b != 0)
+                printf("Resultado: %.2f\n", a / b);
+            else
+                printf("Erro: divisão por zero\n");
+            break;
+        default:
+            printf("Operador inválido\n");
+    }
+
+    return 0;
+}
