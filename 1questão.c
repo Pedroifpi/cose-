de36b5591523fe43f94c498f50d6ecc25d2e04fb
pedro lucas <160865525+Pedroifpi@users.noreply.github.com>
2026-04-30 15:02:07 -0300

    Atualizar o 1questão.c

--- 1questão.c
+++ 1questão.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main() {
+    char c;
+
+    printf("Digite uma letra: ");
+    scanf(" %c", &c);
+
+    switch (c) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            printf("É vogal\n");
+            break;
+        default:
+            printf("Não é vogal\n");
+    }
+
+    return 0;
+}
